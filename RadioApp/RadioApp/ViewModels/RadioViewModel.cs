b97886ec46b7c47ec34e174f06f6eba81d1420fc
Raphael Yaadar <yaadar.5913211@gmail.com>
2023-03-03 00:20:06 +0000

    Add RadioViewModel

---
+++ RadioApp/RadioApp/ViewModels/RadioViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using RadioApp.Annotations;
+
+namespace RadioApp.ViewModels;
+
+public class RadioViewModel : INotifyPropertyChanged
+{
+    bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+    {
+        if (Object.Equals(storage, value))
+            return false;
+
+        storage = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    [NotifyPropertyChangedInvocator]
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
