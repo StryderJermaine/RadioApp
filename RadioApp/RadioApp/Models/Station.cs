0e673da9910ba861370065748e8c9ca28c5eb31f
Raphael Yaadar <yaadar.5913211@gmail.com>
2023-03-03 00:08:19 +0000

    Add station model class

---
+++ RadioApp/RadioApp/Models/Station.cs
@@ -0,0 +1,17 @@
+﻿namespace RadioApp.Models;
+
+/// <summary>
+/// Model class for a radio station
+/// </summary>
+public class Station
+{
+    /// <summary>
+    /// Frequency of the station
+    /// </summary>
+    public double Frequency { get; set; }
+
+    /// <summary>
+    /// Flag to indicate if a station is a favourite or not
+    /// </summary>
+    public bool IsFavourite { get; set; }
+}
